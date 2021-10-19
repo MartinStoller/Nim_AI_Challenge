609c33469cd3e0c5b234bcd4db66b4cdb377364b
MartinStoller <martin.stoller2@gmx.de>
2021-10-19 14:09:52 +0200

    Textinput works now (almost) correctly

--- nim_yt/GUI_nim/gui.py
+++ nim_yt/GUI_nim/gui.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 import nim_yt.businesslayer_nim.pygame_textinput as pg_textinput
 import nim_yt.businesslayer_nim.blocks as nim_blocks
@@ -20,8 +22,10 @@ def main():
 # SET INITIAL PARAMETERS:
     dynamic_line_confirmed_text0 = SMALL_FONT.render("", True, (255, 255, 255))
     dynamic_line_confirmed_text1 = SMALL_FONT.render("", True, (255, 255, 255))
+    dynamic_blocks_confirmed_text0 = SMALL_FONT.render("", True, (255, 255, 255))
+    dynamic_blocks_confirmed_text1 = SMALL_FONT.render("", True, (255, 255, 255))
     line_confirmed = False  # if True, a valid value for the Line from which to remove Blocks was already chosen by player
-    # TODO: line_confirmed and dynamic_texts needs to be reset after each round with a reset() method
+    blocks_confirmed = False  # if True a valid value for the number of Blocks, which are to be removed was already entered
     pygame.display.set_caption("Marty´s Coding Palace: Nim Challenge")
 
     # Create TextInput-objects:
@@ -35,8 +39,10 @@ def main():
 
     # Load initial Text displays:
     textsurface = []
-    textsurface.append((dynamic_line_confirmed_text0, (600, 100)))  # dynamic texts need to be at index 0 and 1!!!
+    textsurface.append((dynamic_line_confirmed_text0, (600, 100)))  # dynamic texts need to be at index 0-3!!!
     textsurface.append((dynamic_line_confirmed_text1, (600, 114)))
+    textsurface.append((dynamic_line_confirmed_text0, (600, 135)))
+    textsurface.append((dynamic_line_confirmed_text1, (600, 149)))
     text0 = FONT.render('How many blocks do you want to', True, (0, 0, 0))  # TODO: replace with a comment about game when it is over (Sth like: Congrats, you won! press Enter if you want to play another round)
     text1 = FONT.render('remove and from which Line?', True, (0, 0, 0))
     text2 = FONT.render("Line:", True, (255, 255, 255))
@@ -52,8 +58,6 @@ def main():
         pygame.draw.rect(SCREEN, WHITE, (850, 55, 50, 35), 2)
         pygame.draw.rect(SCREEN, WHITE, (675, 18, 50, 35), 2)
 
-
-
         nim_blocks.drawAllBlocks(SCREEN, piles)
 
         events = pygame.event.get()
@@ -61,26 +65,28 @@ def main():
             if event.type == pygame.QUIT:
                 exit()
 
-        # Blit input surface onto the screen
-        # get line -> check validity -> if True get blocks if not get line again with error message
-
-        if line_confirmed is False:
-            print(textinput_nim.get_line(
-                textinput_line=textinput_line, screen=screen, events=events, piles=piles,
-                initial_text0=dynamic_line_confirmed_text0, initial_text1=dynamic_line_confirmed_text1))
+        if line_confirmed is False:  # if a valid line as not been chosen yet, wait for valid input:
             line_input, dynamic_line_confirmed_text0, dynamic_line_confirmed_text1 = textinput_nim.get_line(
                 textinput_line=textinput_line, screen=screen, events=events, piles=piles,
                 initial_text0=dynamic_line_confirmed_text0, initial_text1=dynamic_line_confirmed_text1)
-        if line_confirmed is True:
+            textsurface[0] = (dynamic_line_confirmed_text0, (600, 100))
+            textsurface[1] = (dynamic_line_confirmed_text1, (600, 114))
+            if line_input is not None:
+                line_confirmed = True
+
+        if line_confirmed is True and blocks_confirmed is False:  # if a valid line has been chosen but not the Nr of Blocks, wait for valid input:
+            blocks_input, dynamic_blocks_confirmed_text0, dynamic_blocks_confirmed_text1 = textinput_nim.get_blocks(
+                textinput_blocks=textinput_blocks, line=line_input, screen=screen, events=events, piles=piles,
+                initial_text0=dynamic_blocks_confirmed_text0, initial_text1=dynamic_blocks_confirmed_text1)
+            textsurface[2] = (dynamic_blocks_confirmed_text0, (600, 135))
+            textsurface[3] = (dynamic_blocks_confirmed_text1, (600, 149))
+
+        if line_confirmed is True and blocks_confirmed is True:  # if both inputs were entered, make move
             dynamic_line_confirmed_text0 = SMALL_FONT.render("", True, (255, 255, 255))
             dynamic_line_confirmed_text1 = SMALL_FONT.render("", True, (255, 255, 255))
             textsurface[0] = (dynamic_line_confirmed_text0, (600, 100))
             textsurface[1] = (dynamic_line_confirmed_text1, (600, 114))
-
-
-            # NEXT TODO: OLD MESSAGES DO NOT GET DELETED
-
-        # TODO: as moves get executed add the texts from terminal to GUI, which summarize each players move
+            # TODO make_move(), add summary text of move to GUI and (if necessary) reset parameters such as dynamic texts and booleans
 
         for text in textsurface:
             SCREEN.blit(text[0],
--- nim_yt/businesslayer_nim/textinput_nim.py
+++ nim_yt/businesslayer_nim/textinput_nim.py
@@ -12,27 +12,18 @@ def get_line(textinput_line, screen, events, piles, initial_text0, initial_text1
     :param piles: list of available blocks (at the beginning of game [1, 3, 5, 7])
     :return: int: line, from which blocks are supposed to be removed. None if invalid input
 
-    If line confirmed is false, text input is shown under "Line:", and the resulting input is recognized
-        as the line-input and checked for validity accordingly.
-    If valid, it is stored as line_input and line_confirmed
-        switches to True. Additionally a message "You chose line X. how many Blocks would you like to remove from this
-        line?" occurs.
-    If invalid the message "Invalid input. This line cannot be chosen" occures on the GUI, but
-        disappears when a valid input comes in. A similar error message exists for the textinput_blocks.
-    Once a valid input was entered for both, a tuple with both inputs is returned
-
+    checks validity of the input (from which line to remove blocks). If not valid returns None + error messages.
+    If valid it returns the input as int + confirming messages
     """
     screen.blit(textinput_line.get_surface(), (690, 25))
     if textinput_line.update(events):
         line_input = textinput_line.get_text()
 
         if check_validity_line(line_input, piles) is False:
-            text0, text1 = print_message_line(validity=check_validity_line(line_input, piles),
-                                              line_input=999)
+            text0, text1 = print_message_line(validity=False, line_input=999)
             return None, text0, text1
         else:
-            text0, text1 = print_message_line(validity=check_validity_line(line_input, piles),
-                                             line_input=line_input)
+            text0, text1 = print_message_line(validity=True, line_input=line_input)
             return int(line_input), text0, text1
     else:
         return None, initial_text0, initial_text1  # if no even was detected, keep the text the way it was
@@ -54,9 +45,9 @@ def check_validity_line(textinput, piles):
     for e in enumerate(piles):
         if e[1] != 0:
             valid_inputs.append(e[0])
+
     if textinput not in valid_inputs:
         return False
-
     return True
 
 
@@ -71,3 +62,56 @@ def print_message_line(validity, line_input):
         text1 = FONT.render("remove from line " + str(line_input) + "?", True, (255, 255, 255))
 
     return text0, text1
+
+
+def get_blocks(textinput_blocks, line, screen, events, piles, initial_text0, initial_text1):
+    """
+    :param textinput_blocks: TextInput Object
+    :param line: the line, which was chosen to remove blocks from
+    :param screen: gui-screen
+    :param events: detected events by pygame (keystrokes)
+    :param piles: list of available blocks (at the beginning of game [1, 3, 5, 7])
+    :return: int: line, from which blocks are supposed to be removed. None if invalid input
+
+    checks validity of the input (from which line to remove blocks). If not valid returns None + error messages.
+    If valid it returns the input as int + confirming messages
+    """
+    screen.blit(textinput_blocks.get_surface(), (865, 62))
+    if textinput_blocks.update(events):
+        blocks_input = textinput_blocks.get_text()
+        if len(str(blocks_input))>0:
+            if check_validity_blocks(blocks_input, line, piles) is False:
+                text0, text1 = print_message_blocks(validity=False, piles=piles, line=line)
+                return None, text0, text1
+            else:
+                text0, text1 = print_message_blocks(validity=True, piles=piles, line=line)
+                return int(blocks_input), text0, text1
+
+
+    return None, initial_text0, initial_text1
+
+
+
+def print_message_blocks(validity, piles, line):
+    FONT = pygame.font.SysFont('bahnschrift', 15)
+    if validity is False:
+        text0 = FONT.render("Invalid Input! Line " + str(line) + " contains " + str(piles[line]) + " Blocks.",
+                            True, (255, 255, 255))
+        text1 = FONT.render("Please choose a number between 1 and " + str(piles[line]), True, (255, 255, 255))
+    else:
+        text0 = FONT.render("", True, (255, 255, 255))
+        text1 = FONT.render("", True, (255, 255, 255))
+
+    return text0, text1
+
+
+def check_validity_blocks(nr_of_blocks, line, piles):
+    # Check if input can be converted to int:
+    try:
+        nr_of_blocks = int(nr_of_blocks)
+    except:
+        return False
+    # check if input is in allowed range
+    if 0 < nr_of_blocks <= piles[line]:
+        return True
+    return False
